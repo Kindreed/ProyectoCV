3cc2375aade7c672bae6b498bb3e6a349fc6b80a
Kindreed <gabriel.david.sanchez94@gmail.com>
2023-02-26 18:53:07 -0300

    cambios

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById("botonform").addEventListener("click", function(){
+    document.getElementById("eventoform").innerHTML="Sus Datos se han enviado";
+})
+document.getElementById("botonform").addEventListener("click", function(){
+    document.getElementById("eventoform").style.color="black";
+})
